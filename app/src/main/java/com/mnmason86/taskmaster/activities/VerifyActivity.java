47031b6e66c42ae2aefa818aa1d44b40d8aefad3
Mandy Mason <mnmason86@gmail.com>
2022-10-04 20:06:46 -0400

    added new activities for sign-up, log-in, and verify

---
+++ app/src/main/java/com/mnmason86/taskmaster/activities/VerifyActivity.java
@@ -0,0 +1,15 @@
+package com.mnmason86.taskmaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.mnmason86.taskmaster.R;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
