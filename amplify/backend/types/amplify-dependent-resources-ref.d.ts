2403b79ce5ce9e25cd591b4d2bb314bccebe17e3
Mandy Mason <mnmason86@gmail.com>
2022-10-11 12:41:47 -0400

    added S3 through amplify, dependency in build.gradle, and plugin in TaskAmplifyApplication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "taskmasterstorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
